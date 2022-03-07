bad137c20d08ee88852f8e7725b331acc495ce9f
zaniaff <100548269+zaniaff@users.noreply.github.com>
2022-03-07 10:55:46 +0700

    Add files via upload

---
+++ war/war/Monster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace war
+{
+    public class Monster
+    {
+       public int hp = 100;
+       public int power = 200;
+
+        
+    }
+}
