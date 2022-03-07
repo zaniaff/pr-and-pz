e5efeadad85a9aae2aa7c6be74be348baeb33ad8
zaniaff <100548269+zaniaff@users.noreply.github.com>
2022-03-07 12:27:16 +0700

    Add files via upload

---
+++ war/war/Human.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace war
+{
+    public class Human
+    {
+        public string head = "голова";
+        public string tors = "тело";
+        public string heands = "рука";
+        public string legs = "нога";
+    }
+}
